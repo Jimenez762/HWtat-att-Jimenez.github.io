0ac09b13943f1810028d5f969a126639d29f473a
Jimenez762 <vuseckaslava1@gmail.com>
2025-01-03 08:47:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@
     particlesJS("particles-js", particlesConfig);
 	
 	 // Код, который будет проверять правильность введенного пароля
-    const correctPassword = 'samoban';  // Задайте правильный набор букв
+    const correctPassword = 'Bust';  // Задайте правильный набор букв
     const modal = document.getElementById('password-modal');
     const errorMessage = document.getElementById('error-message');
     const passwordInput = document.getElementById('password-input');
